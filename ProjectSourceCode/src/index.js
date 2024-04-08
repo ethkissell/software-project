19988ef1cb2b9e6826dd2f6290a2b605cf26eea9
Kyle Johnson <kyjo1355@colorado.edu>
2024-04-08 16:48:24 -0600

    setup for test cases

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -183,10 +183,15 @@ app.get('/stats1', (req,res) => {
     res.render('pages/stats1');
 })
 
+app.get('/welcome', (req, res) => {
+    res.json({status: 'success', message: 'Welcome!'});
+  });
+
 
 // *****************************************************
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000);
+//app.listen(3000);
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
