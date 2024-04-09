ed74e72dbbd7858d60d9e8d3576dd58f718d8da8
Kyle Johnson <kyjo1355@colorado.edu>
2024-04-08 18:47:15 -0600

    skeleton for register test cases

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -41,25 +41,34 @@ describe('Server!', () => {
 // Explanation: The testcase will call the /add_user API with the following input
 // and expects the API to return a status of 200 along with the "Success" message.
 
-describe('Testing Add User API', () => {
-  it('positive : /add_user', done => {
+/*describe('Testing Register API', () => {
+  it('positive : /register', done => {
     chai
       .request(server)
-      .post('/add_user')
-      .send({id: 5, name: 'John Doe', dob: '2020-02-20'})
+      .post('/register')
+      .send({username: '', email: '', password: ''})
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.message).to.equals('Success');
         done();
       });
   });
-});
+});*/
 
 //We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 400 along with a "Invalid input" message.
 
-describe('Testing Add User API', () => {
-  it('positive : /add_user', done => {
+describe('Testing Register API', () => {
+  it('positive : /register', done => {
     // Refer above for the positive testcase implementation
+    chai
+      .request(server)
+      .post('/register')
+      .send({username: '', email: '', password: ''})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals('Success');
+        done();
+      });
   });
 
   // Example Negative Testcase :
@@ -69,14 +78,14 @@ describe('Testing Add User API', () => {
   // Result: This test case should pass and return a status 400 along with a "Invalid input" message.
   // Explanation: The testcase will call the /add_user API with the following invalid inputs
   // and expects the API to return a status of 400 along with the "Invalid input" message.
-  it('Negative : /add_user. Checking invalid name', done => {
+  it('Negative : /regiser. Checking invalid name', done => {
     chai
       .request(server)
-      .post('/add_user')
-      .send({id: '5', name: 10, dob: '2020-02-20'})
+      .post('/register')
+      .send({username: '', email: '', password: ''})
       .end((err, res) => {
         expect(res).to.have.status(400);
-        expect(res.body.message).to.equals('Invalid input');
+        expect(res.body.message).to.equals('Email does not exist or does not have an existing YouTube account');
         done();
       });
   });
