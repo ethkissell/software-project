ca417ce0277e004237839d04cdf4f5df1bb9bebd
Renata Zurita <renatazurita9@gmail.com>
2024-04-11 01:35:27 -0600

    Sign in with Google

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -24,6 +24,15 @@ const hbs = handlebars.create({
     partialsDir: __dirname + '/views/partials',
 });
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+  
+
 // database configuration
 const dbConfig = {
     host: 'db', // the database server
